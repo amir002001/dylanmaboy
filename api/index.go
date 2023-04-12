8ad556458fa2905687990faf757b8f6011ff48ec
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2023-04-11 22:00:55 -0400

    done done

--- api/index.go
+++ api/index.go
@@ -6,5 +6,5 @@ import (
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<a href=\"https://store.steampowered.com/account/ackgift/1414230BF24B4657?redeemer=\'https://store.steampowered.com/account/ackgift/1414230BF24B4657?redeemer=\">happy birthday bud</a>")
+  fmt.Fprintf(w, "https://store.steampowered.com/account/ackgift/1414230BF24B4657?redeemer=")
 }
